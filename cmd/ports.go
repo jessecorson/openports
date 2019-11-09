0fa7e7bcc3fa7b6718468067bb18dfd9a044cb2a
Jesse Corson <jessecorson@gmail.com>
2019-11-09 17:22:22 -0500

    changed to one flag

--- cmd/ports.go
+++ cmd/ports.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -34,12 +35,10 @@ func makeRange(min, max int) []int {
 	return a
 }
 
-func openports(r []int) {
+func openports(p []string) {
 
-	f := r[0]
-	l := r[1]
+	a := makePortList(p)
 
-	a := makeRange(f, l)
 	http.HandleFunc("/", handler)
 	for _, p := range a {
 		// fmt.Println(p)
@@ -51,4 +50,37 @@ func openports(r []int) {
 	// Keep process running
 	for {
 	}
+}
+
+func makePortList(rSlice []string) []int {
+
+	var pRange []int
+	// Checking if port is an int
+	if len(rSlice) < 2 {
+		r := rSlice[0]
+		p, err := strconv.Atoi(r)
+		if err != nil {
+			// Checking if range
+			rSplit := strings.Split(r, "-")
+			if !(len(rSplit) > 2) {
+				for _, a := range rSplit {
+					if i, err := strconv.Atoi(a); err == nil {
+						pRange = append(pRange, i)
+					}
+				}
+				f := pRange[0]
+				l := pRange[1]
+				pRange := makeRange(f, l)
+				return pRange
+			}
+		}
+		pRange = []int{p}
+		return pRange
+	}
+	for _, a := range rSlice {
+		if i, err := strconv.Atoi(a); err == nil {
+			pRange = append(pRange, i)
+		}
+	}
+	return pRange
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"strconv"
-	"strings"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -17,7 +14,7 @@ var (
 		Short: "openports, get nauti",
 		Long:  `openports is a firewall testing tool. It allows you to create a range of listening ports.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			start()
+			openports(viper.GetStringSlice("port"))
 		},
 	}
 )
@@ -28,34 +25,6 @@ func Execute() error {
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringP("range", "r", "int-int", "Port Range")
-	rootCmd.PersistentFlags().IntP("first", "f", 80, "First Port")
-	rootCmd.PersistentFlags().IntP("last", "l", 0, "Last Port")
-	viper.BindPFlag("range", rootCmd.PersistentFlags().Lookup("range"))
-	viper.BindPFlag("first", rootCmd.PersistentFlags().Lookup("first"))
-	viper.BindPFlag("last", rootCmd.PersistentFlags().Lookup("last"))
-}
-
-func getRange(r string) []int {
-	var pRange []int
-	if r != "int-int" {
-		rSplit := strings.Split(r, "-")
-		if len(rSplit) > 2 {
-			return []int{80}
-		}
-		for _, a := range rSplit {
-			if i, err := strconv.Atoi(a); err == nil {
-				pRange = append(pRange, i)
-			}
-		}
-	}
-	return pRange
-}
-
-func start() {
-	portRange := getRange(viper.GetString("range"))
-	if len(portRange) < 1 {
-		portRange = []int{viper.GetInt("first"), viper.GetInt("last")}
-	}
-	openports(portRange)
+	rootCmd.PersistentFlags().StringSliceP("port", "p", []string{"0"}, "Port, Port range or Port list")
+	viper.BindPFlag("port", rootCmd.PersistentFlags().Lookup("port"))
 }
